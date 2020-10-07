bfce9b50538b77375fe46405e5630bb038c556b7
uta-mori <mori_yuta@retail-ai.jp>
2020-10-07 09:32:43 +0900

    ♻️ ginに変更

--- main.go
+++ main.go
@@ -1,35 +1,13 @@
-// Sample run-helloworld is a minimal Cloud Run service.
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
-
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
-	}
-
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "World"
-	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
